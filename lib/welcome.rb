681eb22f86dfdf99b895d63ad12320880c976247
Harriet Bicknell <harrietindubai@gmail.com>
2019-10-24 12:02:31 +0000

    Done.

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,1 +1,14 @@
 puts "Welcome to Tic Tac Toe!"
+
+first_number = 7
+second_number = 14
+ 
+sum = first_number + second_number
+ 
+puts sum
+
+current_president = "Barack Obama"
+puts "In 2016, the president was #{current_president}."
+
+current_president = "Stephen Colbert"
+puts "Now, it being the year 2017, the president is #{current_president}."
